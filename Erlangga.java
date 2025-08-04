9b4e64b4141f581ce56c23b073a4cd94878339b8
erlanggadwiRPL <erlangga_firmansah_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 13:17:52 +0700

    Add files via upload

---
+++ Erlangga.java
@@ -0,0 +1,13 @@
+public class Erlangga {
+    public static void main(String[] args) {
+     //main metod
+    System.out.println("Erlangga Dwi Firmansah");
+    System.out.println("SMK Telkom Malang");
+    System.out.println("SMP Kartika IV-8");
+    System.out.println("Deni Dwi Santo");
+    System.out.println("Rini Pujiati");
+    System.out.println("Menjadi orang sukses");
+    System.out.println("Futsal");
+}
+
+}
