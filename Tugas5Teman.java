65fb6293dc363e14219e4157c03b18e454b02000
erlanggadwiRPL <erlangga_firmansah_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 14:53:54 +0700

    Add files via upload

---
+++ Tugas5Teman.java
@@ -0,0 +1,15 @@
+public class Tugas5Teman {
+    public static void main(String[] args) {
+        //nama teman
+        String namasiswa ="Muhammad gavrila Rafif prayata";
+        String namasiswa ="Muhamad Jericho Husein";
+        String namasiswa ="Atha koirun ayyasi";
+        String namasiswa ="Intazar Lintanng Pratama";
+        String namasiswa ="Sakti mahaya zaman";
+
+
+
+    }
+
+
+}
